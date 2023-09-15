5f055d4cc1e609fa511e2cccf492d9287ab0f31c
Lucas Pairé <lxpaire@inf.ufsm.br>
2023-09-15 07:42:29 -0300

    teste

---
+++ trab3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct clientes{
+    int telefone;
+    char cnh[20];
+    char nome[100];
+    struct clientes* prox;
+};
+typedef struct clientes Cliente;
+
+void cria_lista(){
+    return NULL;
+}
+
+Cliente* insere_clientes(Cliente* l, int telefone, char cnh, char nome){
+    
+}
+
+int main(){
+    Cliente* a;
+
+}
